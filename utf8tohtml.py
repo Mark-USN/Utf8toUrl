7d192da6ae9647c162c63231381a676456dddcb3
Mark-USN <mark.henwood@gmail.com>
2025-02-20 13:42:15 -0800

    Update utf8tohtml.py

--- utf8tohtml.py
+++ utf8tohtml.py
@@ -2,10 +2,10 @@
 import sys
 
 def unicode_to_hex(text):
-    return '%'.join(f"{ord(char):x}" for char in text)
+    return '%'.join(f"{ord(char):x};" for char in text)
 
 def utf8_to_hex(text):
-    return '%' + '%'.join(f"{byte:02x}" for byte in text.encode('utf-8'))
+    return '%' + '%'.join(f"{byte:02x};" for byte in text.encode('utf-8'))
 
 def main():
     if len(sys.argv) < 2:
